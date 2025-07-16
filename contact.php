1933dc17a3983a622a224fa8cca5b6ae9c833ce1
Anthony1048 <dcorbin104@students.ccsnh.edu>
2025-07-16 09:42:04 -0400

    Initial push from local development environment.

---
+++ contact.php
@@ -0,0 +1,55 @@
+<?php
+$secret = "6LdivFsrAAAAAEBoXjj8iab0QmcmPjxjbS6LaqXs";
+
+if(isset($_POST['name']))  { $name=$_POST['name']; }
+
+if(isset($_POST['email']))  { $email=$_POST['email']; }
+
+if(isset($_POST['message']))  { $message=$_POST['message']; }
+
+if(isset($_POST['subject']))  { $subject=$_POST['subject']; }
+
+if(isset($_POST['g-recaptcha-response']))  { $captcha=$_POST['g-recaptcha-response'];  }
+
+
+if (!$captcha) {
+          echo '<h2>Please check the the captcha form.</h2>';
+              exit;
+           }
+
+
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+
+
+
+$args = [
+	'name' => ['filter' => FILTER_VALIDATE_REGEXP, 'options' => [ 'regexp' => '/^[\pL\pP\pZ]{3,50}$/u']],
+	'email' => ['filter' => FILTER_VALIDATE_EMAIL, 'options' => [ FILTER_FLAG_EMAIL_UNICODE ]],
+	'message' => ['filter'=> FILTER_VALIDATE_REGEXP, 'options' => [ 'regexp' => '/^[^<>]{10,500}$/' ]],
+        'subject' =>  ['filter' => FILTER_VALIDATE_REGEXP, 'options' => [ 'regexp' => '/^[^<>]{5,50}$/' ]],
+];
+
+
+$results = filter_input_array(INPUT_POST,$args);
+
+if (empty($results) || in_array(false,$results,true)){
+	header("Location: /contact.html");
+exit;
+}
+
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli("localhost","anthony", "Ares1234!", "contact");
+
+
+$stmt = $mysqli->prepare("INSERT INTO contact_info (name, email, subject, message) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $results['name'],$results['email'],$results['subject'],$results['message']);
+
+$stmt->execute();
+
+$stmt->close();
+$mysqli->close();
+
+header("Location: /thanks.html")
+
+?>
---
+++ contactdata.php
@@ -0,0 +1,65 @@
+<?php
+ini_set('display_errors', 1);
+$link = mysqli_connect("localhost","anthony", "Ares1234!", "contact");
+
+if (mysqli_connect_errno()){
+printf("Connect failed: %s\n", mysqli_connect_error());
+exit();
+}
+$contacts = [];
+$stmt = mysqli_stmt_init($link);
+
+if (mysqli_stmt_prepare($stmt, "SELECT * FROM contact_info")) {
+
+
+    mysqli_stmt_bind_result($stmt, $id, $name, $email, $subject, $message);
+
+    mysqli_stmt_execute($stmt);
+
+
+    while (mysqli_stmt_fetch($stmt)) {
+   	 $contacts[] = ['id' =>$id, 'name'=> $name, 'email'=> $email, 'subject'=> $subject, 'message'=> $message];
+    }
+
+    mysqli_stmt_close($stmt);
+}
+
+mysqli_close($link);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Data</title>
+</head>
+<body>
+
+  <table>
+    <thead>
+    <tr>
+
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Subject</th>
+      <th>Message</th>
+
+    </tr>
+    </thead>
+
+    <tbody>
+    <?php foreach ($contacts as $row): ?>
+      <tr>
+        <td><?= htmlentities ($row['id']) ?></td>
+        <td><?= htmlentities ($row['name']) ?></td>
+        <td><?= htmlentities ($row['email']) ?></td>
+        <td><?= htmlentities ($row['subject']) ?></td>
+        <td><?= htmlentities ($row['message']) ?></td>
+      </tr>
+    <?php endforeach ?>
+    </tbody>
+
+  </table>
+</body>
+</html>
