c5a1e18b7e8a9cfd6398456c04f11a7abc52715d
Anthony1048 <dcorbin104@students.ccsnh.edu>
2025-07-21 10:13:55 -0400

    Testing gitignore.

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$secret = "6LdivFsrAAAAAEBoXjj8iab0QmcmPjxjbS6LaqXs";
+$secret = "6LfZaoorAAAAAJyUVaACv4ZR88JwknE-XGQZSrHf";
 
 $mysqli = new mysqli("localhost","anthony", "Ares1234!", "contact");
 
